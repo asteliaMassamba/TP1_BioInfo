baf65124c454532c42c475e76aa0243e01a688b1
halex02 <halex02@msn.com>
2016-02-03 14:20:41 +0100

    fin script

--- BI-TOULOUSE-MASSAMBA/execute-tp1.sh
+++ BI-TOULOUSE-MASSAMBA/execute-tp1.sh
@@ -7,11 +7,37 @@ FILE2=../data/test2.fasta
 cd src
 javac *.java
 
-java PrintFastaSequence $FILE0
-java PrintFastastats $FILE0
-java ListKmer 4 $FILE1
-java CommonKmers 4 $FILE1 $FILE2
-java RatioCommonKmers 4 $FILE1 $FILE2
-java RandomMutation 2 $FILE1
+echo print fasta sequence
 
+java PrintFastaSequence $FILE0
+echo
+echo print fasta stats
+
+java PrintFastastats $FILE0
+echo
+echo list kmer
+
+java ListKmer 4 $FILE1
+echo
+echo common kmer
+
+java CommonKmers 4 $FILE1 $FILE2
+echo
+echo ratio common kmer
+
+java RatioCommonKmers 4 $FILE1 $FILE2
+echo
+echo random mutation 1er execution
+
+java RandomMutations 2 $FILE1
+echo
+echo random mutation 2nd execution
+
+java RandomMutations 2 $FILE1
+echo
+echo random mutation 3eme execution
+
+java RandomMutations 2 $FILE1
+echo
+cd ..
 $SHELL
